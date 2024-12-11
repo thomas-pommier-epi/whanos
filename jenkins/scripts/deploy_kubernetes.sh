871b88527e361c0cedf5abf669f1882e3418ccde
thomas-pommier-epi <thomas.pommier@epitech.eu>
2024-12-11 19:53:55 +0100

    fixed kubernetes: missing loadbalancer service for external connections

--- jenkins/scripts/deploy_kubernetes.sh
+++ jenkins/scripts/deploy_kubernetes.sh
@@ -7,16 +7,40 @@ fi
 project_name=$1
 project_id=$(cat /opt/auth/project_id)
 
-exported=$(python3 /opt/k8s/deployer.py "$(realpath .)" $project_name $project_id)
+declare -a exported=($(python3 /opt/k8s/deployer.py "$(realpath .)" $project_name $project_id))
 
-if [[ -z $exported ]]; then
+if [[ ${#exported[@]} -eq 0 ]]; then
     exit 1
 fi
 
-will_restart=$(! kubectl get pods | grep -q $project_name; echo $?)
+will_restart_deploy=$(kubectl get pods | grep -q $project_name; echo $?)
+will_restart_service=$(kubectl get services | grep -q $project_name; echo $?)
 
-kubectl apply -f $exported
+kubectl apply -f ${exported[0]}
+kubectl apply -f ${exported[1]}
 
-if [[ $will_restart -eq 0 ]]; then
+if [[ $will_restart_deploy -eq 0 ]]; then
     kubectl rollout restart deployment/$project_name
 fi
+
+if [[ $will_restart_service -eq 0 ]]; then
+    kubectl rollout restart service/$project_name
+fi
+
+external_ip=""
+tries=0
+max_tries=20
+
+while [ -z "$external_ip" ] && [ $tries -lt $max_tries ]; do
+    echo "Waiting for external IP... (try $((tries+1))/$max_tries)"
+    external_ip=$(kubectl get svc $project_name --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+    [ -z "$external_ip" ] && sleep 5
+    tries=$((tries+1))
+done
+
+if [ -z "$external_ip" ]; then
+    echo "Failed to get external IP after $max_tries tries."
+    exit 1
+fi
+
+echo "External IP: $external_ip"
--- kubernetes/deployer.py
+++ kubernetes/deployer.py
@@ -2,6 +2,18 @@ import yaml
 from jinja2 import Template
 import sys
 
+def render_template(template_lines, data):
+    template = Template(template_lines)
+    return template.render(data)
+
+def write_template(project_name, parsed, mode):
+    export_path = f'/tmp/{project_name}.{mode}.yml'
+    with open(export_path, 'w') as f:
+        f.write(parsed)
+    return export_path
+
+####################
+
 if len(sys.argv) != 4:
     print("Usage: python deployer.py <path/to/whanos.yml> <project_name> <project_id>", file=sys.stderr)
     sys.exit(1)
@@ -10,9 +22,6 @@ project_path = sys.argv[1]
 project_name = sys.argv[2]
 project_id = sys.argv[3]
 
-with open('/opt/k8s/template.deployment.yml.j2', 'r') as f:
-    k8s_template = f.read()
-
 with open(f"{project_path}/whanos.yml", 'r') as f:
     data = yaml.safe_load(f)
 
@@ -20,14 +29,18 @@ if not data:
     print("The file 'whanos.yml' is empty", file=sys.stderr)
     sys.exit(1)
 
+with open('/opt/k8s/template.deployment.yml.j2', 'r') as f:
+    deployment_template = f.read()
+
+with open('/opt/k8s/template.service.yml.j2', 'r') as f:
+    service_template = f.read()
+
 data['name'] = project_name
-data['project_name'] = f"gcr.io/{project_id}/{project_name}"
+data['image_name'] = f"gcr.io/{project_id}/{project_name}"
 
-template = Template(k8s_template)
-rendered_yaml = template.render(data)
+parsed_deployments = render_template(deployment_template, data)
+export_deployments = write_template(project_name, parsed_deployments, 'deployment')
+parsed_service = render_template(service_template, data)
+export_service = write_template(project_name, parsed_service, 'service')
 
-export_path = f'/tmp/{project_name}.deployment.yml'
-with open(export_path, 'w') as f:
-    f.write(rendered_yaml)
-
-print(export_path)
+print(f"{export_deployments} {export_service}")
